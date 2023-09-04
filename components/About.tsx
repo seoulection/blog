f6fd9c8ac4b0f01408c193cd6d07d742d593b9fa
seoulection <6445410+seoulection@users.noreply.github.com>
2023-09-04 10:25:02 -0700

    adds strava and styling fixes

--- components/About.tsx
+++ components/About.tsx
@@ -5,6 +5,7 @@ const GITHUB = 'https://www.github.com/seoulection'
 const INSTAGRAM = 'https://www.instagram.com/cloud__n9ne'
 const LINKEDIN = 'https://www.linkedin.com/in/brian-sung-bb08ba106'
 const SOUNDCLOUD = 'https://soundcloud.com/cloud_9000'
+const STRAVA = 'https://www.strava.com/athletes/82790370'
 
 export default function About(): JSX.Element {
   return (
@@ -14,6 +15,7 @@ export default function About(): JSX.Element {
       <a href={INSTAGRAM} rel="noopener noreferrer" target="_blank">instagram</a>
       <a href={LINKEDIN} rel="noopener noreferrer" target="_blank">linkedin</a>
       <a href={SOUNDCLOUD} rel="noopener noreferrer" target="_blank">soundcloud</a>
+      <a href={STRAVA} rel="noopener noreferrer" target="_blank">strava</a>
     </section>
   )
 }
